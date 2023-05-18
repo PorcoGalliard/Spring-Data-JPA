2ed8e951b3beb90c6a7c0c604bad5051ef285b8e
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-18 20:23:19 +0700

    feat: create JPQL for searching student with certain first name and age

--- src/main/java/com/example/springdatajpa/StudentRepository.java
+++ src/main/java/com/example/springdatajpa/StudentRepository.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query("SELECT s from Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
+    @Query("SELECT s from Student s WHERE s.firstName = ?1 AND s.age =?2 ")
     List<Student> findStudentsByFirstNameEqualsAndAgeEquals(
             String name, Integer age);
     List<Student> findStudentsByFirstNameEqualsAndAgeGreaterThan(
