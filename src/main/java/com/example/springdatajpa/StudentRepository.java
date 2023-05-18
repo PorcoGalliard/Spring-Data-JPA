928dd10fc16dd57bc6c37de85c1b688910bb6bf8
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-18 20:59:45 +0700

    feat: create JPQL for deleting student by ID

--- src/main/java/com/example/springdatajpa/StudentRepository.java
+++ src/main/java/com/example/springdatajpa/StudentRepository.java
@@ -1,8 +1,10 @@
 package com.example.springdatajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,4 +25,9 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
             @Param("firstName") String firstName,
             @Param("age") Integer age
     );
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Student s WHERE s.id = ?1")
+    int deleteStudentById(Long id);
 }
