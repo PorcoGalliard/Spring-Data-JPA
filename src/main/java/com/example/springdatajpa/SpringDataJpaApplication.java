87d14b9c60474aef4ff8b6f23c02d0ef72e5fdb5
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-19 06:55:04 +0700

    feat: use Faker to generate students and implement sorting

--- src/main/java/com/example/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/example/springdatajpa/SpringDataJpaApplication.java
@@ -1,13 +1,51 @@
 package com.example.springdatajpa;
 
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataJpaApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            generateStudents(studentRepository);
+            Sort sort = Sort.by("firstName").ascending()
+                    .and(Sort.by("age").descending());
+            studentRepository
+                    .findAll(sort)
+                    .forEach(student -> System.out.println(
+                            student.getFirstName() +
+                            " " +
+                            student.getAge()));
+        };
+    }
+
+    private static void generateStudents(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for (int i = 0; i < 20; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
+            int age = faker.number().numberBetween(17, 55);
+            Student student = new Student(
+                    firstName,
+                    lastName,
+                    email,
+                    age
+            );
+            studentRepository.save(student);
+        }
     }
 
 }
