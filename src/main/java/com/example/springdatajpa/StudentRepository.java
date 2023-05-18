a67301d8a6234b43d84840e50bf4b324664dfd88
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-18 20:10:42 +0700

    feat: create new custom query method in StudentRepository

--- src/main/java/com/example/springdatajpa/StudentRepository.java
+++ src/main/java/com/example/springdatajpa/StudentRepository.java
@@ -9,4 +9,6 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findStudentByEmail(String email);
     List<Student> findStudentsByFirstNameEqualsAndAgeEquals(
             String name, Integer age);
+    List<Student> findStudentsByFirstNameEqualsAndAgeGreaterThan(
+            String name, Integer age);
 }
