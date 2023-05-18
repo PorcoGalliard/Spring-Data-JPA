1b099d2a6f741b1a63227a820072aeeda01536b0
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-18 20:20:10 +0700

    feat: create JPQL on search student by email

--- src/main/java/com/example/springdatajpa/StudentRepository.java
+++ src/main/java/com/example/springdatajpa/StudentRepository.java
@@ -1,11 +1,13 @@
 package com.example.springdatajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s from Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
     List<Student> findStudentsByFirstNameEqualsAndAgeEquals(
             String name, Integer age);
