a8694e930b5cdffbfff2bdef6389e8b67ce3b5ce
PorcoGalliard <nanasuryana335@gmail.com>
2023-05-20 00:05:56 +0700

    refactor: give parameter while extending the CrudRepository class

--- src/main/java/com/example/springdatajpa/StudentIdCardRepository.java
+++ src/main/java/com/example/springdatajpa/StudentIdCardRepository.java
@@ -2,6 +2,6 @@ package com.example.springdatajpa;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface StudentIdCardRepository extends CrudRepository {
+public interface StudentIdCardRepository extends CrudRepository<StudentIdCard, Long> {
 
 }
